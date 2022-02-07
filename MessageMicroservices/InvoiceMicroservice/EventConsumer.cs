69e0fd36d27ab8c821793844da93a6a4f2f364b5
Nikita Luksha <nikita.luksha@softensity.com>
2022-02-07 20:39:24 +0300

    Add InvoiceMicroservice

---
+++ MessageMicroservices/InvoiceMicroservice/EventConsumer.cs
@@ -0,0 +1,32 @@
+﻿using MassTransit;
+using MessageContracts;
+
+namespace InvoiceMicroservice
+{
+    internal class EventConsumer : IConsumer<IInvoiceToCreate>
+    {
+        public async Task Consume(ConsumeContext<IInvoiceToCreate> context)
+        {
+            var newInvoiceMessage = new Random().Next(10000, 99999);
+            var customer = context.Message.CustomerNumber;
+            Console.WriteLine($"Creating invoice {newInvoiceMessage} for customer: {customer}");
+
+            context.Message.InvoiceItems.ForEach(i =>
+            {
+                Console.WriteLine($"With items: Price: {i.Price}, Desc: {i.Description}");
+                Console.WriteLine($"Actual distance i miles: {i.ActualMileage}, Base Rate: {i.BaseRate}");
+                Console.WriteLine($"Oversized: {i.IsOversized}, Refrigerated: {i.IsRefrigerated}, HazMat: {i.IsHazardousMaterial}");
+            });
+
+            await context.Publish<IInvoiceCreated>(new
+            {
+                InvoiceNumber = newInvoiceMessage,
+                InvoiceData = new
+                {
+                    context.Message.CustomerNumber,
+                    context.Message.InvoiceItems
+                }
+            });
+        }
+    }
+}
---
+++ MessageMicroservices/InvoiceMicroservice/Program.cs
@@ -0,0 +1,35 @@
+﻿using GreenPipes;
+using InvoiceMicroservice;
+using MassTransit;
+using MessageContracts;
+
+const int RETRY_COUNT = 5;
+const int RETRY_INTERVAL_SECONDS = 10;
+const int CANCELLATION_DELAY_SECCONDS = 10;
+const int LISTENING_DELAY_MILLISECONDS = 100;
+
+var busControll = Bus.Factory.CreateUsingRabbitMq(config =>
+{
+    config.Host("localhost");
+    config.ReceiveEndpoint("invoice-service", e =>
+    {
+        e.UseInMemoryOutbox();
+        e.Consumer<EventConsumer>(c => c.UseMessageRetry(m => m.Interval(RETRY_COUNT, new TimeSpan(0, 0, RETRY_INTERVAL_SECONDS))));
+    });
+});
+var source = new CancellationTokenSource(TimeSpan.FromSeconds(CANCELLATION_DELAY_SECCONDS));
+
+await busControll.StartAsync(source.Token);
+Console.WriteLine("Invoice Microservice Now Listening");
+
+try
+{
+    while (true)
+    {
+        await Task.Delay(LISTENING_DELAY_MILLISECONDS);
+    }
+}
+finally
+{
+    await busControll.StopAsync();
+}
